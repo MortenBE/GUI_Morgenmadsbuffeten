60ba5097a0dc2e6a543afd9c243cf792d57668c7
MortenBE <Ehlersmorten@gmail.com>
2020-04-20 12:24:44 +0200

    Not working yet

--- Buffet_GUI/Controllers/KitchenController.cs
+++ Buffet_GUI/Controllers/KitchenController.cs
@@ -1,5 +1,6 @@
 ﻿using Buffet_GUI.Data;
 using Buffet_GUI.Data.DBModels;
+using Buffet_GUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,34 @@ namespace Buffet_GUI.Controllers
             //var restaurantCheckIns = _context.Set<BuffetReservation>().Where(c => c.Date == date).ToList();
 
             return View(restaurantCheckIns);
+        }
+
+        /* Given date, show guest-information (expected amount, checked-in amount, etc.) */
+        public IActionResult ShowNotCheckInGuests()
+        {
+          
+            DateTime date = DateTime.Now.Date;
+
+            var expectedGuestsInfo = _context.Set<BuffetReservation>().Find(date.Day);
+
+            var checkedInEntries = _context.Set<CheckedInGuest>().Where(c => c.Date.Day == date.Day).ToList();
+
+            //var buffetReservations = _context.Set<BuffetReservation>().Where(c => (c.Date.Day == date.Day) && (c.Date.Month == date.Month) && (c.Date.Year == date.Year)).ToList();
+            //var restaurantCheckIns = _context.Set<CheckedInGuest>().Where(c => c.Date == date).ToList();
+
+            var vm = new KitchenViewModel(expectedGuestsInfo, checkedInEntries);
+
+            return View(vm);
+
+
+            //    if (buffetReservations != null && restaurantCheckIns.Count > 0)
+            //    {
+            //        var vm = new KitchenViewModel(buffetReservations, restaurantCheckIns);
+
+            //        return View(vm);
+            //    }
+
+            //    return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Buffet_GUI/Models/KitchenViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Buffet_GUI.Data.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet_GUI.Models
+{
+    public class KitchenViewModel
+    {
+        public KitchenViewModel(BuffetReservation expectedGuestsInfo, List<CheckedInGuest> checkedInEntries)
+        {
+            var TotalChildren = 0;
+            var TotalAdults = 0;
+            var TotalCheckedIn = 0;
+            
+            checkedInEntries.ForEach(g =>
+            {
+                TotalChildren += g.NumberOfChildrenCheckedIn;
+                TotalAdults += g.NumberOfAdultsCheckedIn;
+            });
+
+            TotalCheckedIn = TotalAdults + TotalChildren;
+
+            TotalChildrenNotCheckedIn = expectedGuestsInfo.NumberOfChildren - TotalChildren;
+            TotalAdultsNotCheckedIn = expectedGuestsInfo.NumberOfAdults - TotalAdults;
+            TotalNotCheckedIn = expectedGuestsInfo.AllExpectedGuests - TotalCheckedIn;
+
+        }
+        public int TotalChildrenNotCheckedIn { get; set; }
+        public int TotalAdultsNotCheckedIn { get; set; }
+        public int TotalNotCheckedIn { get; set; }
+
+    }
+}
